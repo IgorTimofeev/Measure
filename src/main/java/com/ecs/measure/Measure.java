e883fb2f0408c07d80cb118f1d223316a1186d10
Igor Timofeev <methodman1@mail.ru>
2018-06-25 13:28:13 +0300

    Тестируем эту vcs-залупу идейкину

--- src/main/java/com/ecs/measure/Measure.java
+++ src/main/java/com/ecs/measure/Measure.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 public class Measure {
     public static final String MODID = "measure";
     public static final String NAME = "Measure";
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "1.0.0";
     
     public static Minecraft minecraft;
     
